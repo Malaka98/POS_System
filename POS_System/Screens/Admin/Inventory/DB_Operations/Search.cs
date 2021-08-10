d9672893428e6822f7dfaa01fffdf3079595c801
Malaka98 <malaka.aruna14413@gmail.com>
2021-08-10 18:46:39 +0530

    Updated

---
+++ POS_System/Screens/Admin/Inventory/DB_Operations/Search.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace POS_System.Screens.Admin.Inventory.DB_Operations
+{
+    internal class Search
+    {
+        private readonly DBConnection connectionOBJ = null;
+        private SqlDataAdapter adapt = null;
+        private bool disposedValue;
+
+        public Search()
+        {
+            connectionOBJ = DBConnection.GetConnection();
+        }
+
+        public DataTable Search_Query(string category)
+        {
+            try
+            {
+                connectionOBJ.GetConn().Open();
+                DataTable dt = new DataTable();
+                adapt = new SqlDataAdapter("SELECT * FROM Product WHERE Category='" + category + "'", connectionOBJ.GetConn());
+                _ = adapt.Fill(dt);
+                return dt;
+            }
+            catch (SqlException e)
+            {
+                _ = MessageBox.Show(e.ToString());
+                return null;
+            }
+            finally
+            {
+                adapt.Dispose();
+                connectionOBJ.GetConn().Close();
+            }
+
+        }
+
+    }
+}
--- POS_System/Screens/Admin/Inventory/Inventory.xaml.cs
+++ POS_System/Screens/Admin/Inventory/Inventory.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using POS_System.Screens.Admin.Inventory.DB_Operations;
 using System;
 using System.Data;
+using System.Diagnostics;
 using System.Windows.Controls;
 using Report = POS_System.Screens.Admin.Inventory.Report;
 
@@ -60,6 +61,20 @@ namespace POS_System.Screens.Admin.Inventory
         {
             Report::Inventry_Report obj = new Report.Inventry_Report();
             obj.Show();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                string category = (e.AddedItems[0] as ComboBoxItem).Content as string;
+                Search obj = new Search();
+                grid_Inventry.ItemsSource = obj.Search_Query(category).DefaultView;
+
+            }catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
         }
     }
 }
